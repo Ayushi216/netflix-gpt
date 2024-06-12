e6497a4b15fb3e2df95bc0dee83260f4b54faa13
Ayushi Sarkar <ayushisarkar44@gmail.com>
2024-06-12 22:13:36 +0530

    sign out, update state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,39 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/")
+      })
+      .catch((error) => {
+        navigate("/error")
+      });
+  };
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
-      <img className="w-44"
+    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <img
+        className="w-44"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
+      <div className="flex p-3">
+        <img
+          className="w-12 h-12"
+          src="https://occ-0-2232-3662.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABUMx6z-7bB7tyN-OZXt6i8BXuZHN5EWBr7MQy7ilhubrpI2yOofVtT-QmoO6VJt7I1ewosmuQa29BGFfOOVcJxdKx1sT-co.png?r=201
+"
+          alt="user-icon"
+        ></img>
+        <button className="ml-2 font-bold text-white" onClick={handleSignOut}>
+          (Sign Out)
+        </button>
+      </div>
     </div>
   );
 };
